60a01250952dd13ea2fc6689e829d1246da9ec9d
brianmyer <myer.brian@gmail.com>
2023-07-25 13:45:11 -0400

    added arrays for letters, numbers, and symbols

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,45 @@
 // Assignment code here
-
+let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '<', '>', '?', "'", '"'];
+let builtArray = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword (){
+
+if (confirm('Do you want your password to contain lowercase letters?')) {
+  builtArray = [...builtArray, ...lowerAlphabet];
+};
+console.log(builtArray)
+if (confirm('Do you want your password to contain uppercase letters?')) {
+  builtArray = [...builtArray, ...upperAlphabet];
+ 
+};
+console.log(builtArray)
+
+if (confirm('Do you want your password to contain  numbers?')) {
+  builtArray = [...builtArray, ...numbers];
+
+};
+console.log(builtArray)
+
+if (confirm('Do you want your password to contain  special characters?')) {
+  builtArray = [...builtArray, ...specialCharacters];
+
+};
+console.log(builtArray);
+
+let password = "";
+
+for (let i = 0; i < prompt('How many characters would you like your password to be?'); i++) {
+  password = math.floor(math.random()*builtArray.length);
+};
+
+return password;
+}
 
 // Write password to the #password input
 function writePassword() {
